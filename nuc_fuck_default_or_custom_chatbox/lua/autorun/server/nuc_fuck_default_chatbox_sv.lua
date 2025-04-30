1279fa8d840e383e1f72835e57a39ce58dc0aaf3
YohSambre <YohSambre@users.noreply.github.com>
2025-04-30 19:15:26 +0200

    Update nuc_fuck_default_chatbox_sv.lua

--- nuc_fuck_default_or_custom_chatbox/lua/autorun/server/nuc_fuck_default_chatbox_sv.lua
+++ nuc_fuck_default_or_custom_chatbox/lua/autorun/server/nuc_fuck_default_chatbox_sv.lua
@@ -4,3 +4,8 @@
 MsgC (Color(255, 255, 255), [[                                                                                                             
 = NUC - Fuck Default Chatbox loaded!
 ]])
+
+-- DONT BYPASS MY SHEEEEEEEEEEEEEEEEEEEEEEEEEEEET
+hook.Add( "PlayerSay", "NUCHIDEDEFAULTCHATBOXNOBYPASS", function( ply, text )
+	return " "
+end)
